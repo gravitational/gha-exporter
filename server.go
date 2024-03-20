9381d0b0f261af244a38028bff050130e2d8429e
Fred Heinecke <fred.heinecke@goteleport.com>
2024-03-20 12:51:37 -0500

    Added redirect for `/` to metrics endpoint

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 func RunServer(cfg *CLI) error {
 	mux := http.NewServeMux()
 	mux.Handle("GET /metrics", promhttp.Handler())
+	mux.Handle("GET /", http.RedirectHandler("/metrics", http.StatusPermanentRedirect))
 	s := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
